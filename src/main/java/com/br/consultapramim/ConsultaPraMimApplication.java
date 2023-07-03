56f627b2fbf369ddcb8e73e735128cf09cf81829
Lucas Vinicius Silva Cerqueira de Souza <lucasvscs@outlook.com>
2023-07-02 22:54:50 -0300

    Push inicial do projeto

---
+++ src/main/java/com/br/consultapramim/ConsultaPraMimApplication.java
@@ -0,0 +1,13 @@
+package com.br.consultapramim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaPraMimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaPraMimApplication.class, args);
+	}
+
+}
